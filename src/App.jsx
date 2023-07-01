eff53290e8aabf09eb111209d3bddccc760735e9
NIKET KUMAR MISHRA <niketvns97@gmail.com>
2023-07-01 18:32:02 +0530

    vite-react-tailwind-setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='text-center py-5 font-bold text-2xl'>Machine Coding Round - 5</p>
+    </>
+  )
+}
+
+export default App
